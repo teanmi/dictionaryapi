f9a15575880cfc1e8669926f7101365de35b84c0
teanmi <teanmi6@gmail.com>
2022-10-24 21:28:39 -0400

    added link to first website + word styling

--- word.js
+++ word.js
@@ -1,11 +1,22 @@
-let error;
-
-const searchWord = async () => {
-  error = false;
+const searchWord = async (word = "") => {
   const wordsHTML = document.querySelector(".words");
-  wordsHTML.innerHTML = "";
 
-  const word = document.getElementById("search-bar").value.toLowerCase();
+  wordsHTML.removeChild(wordsHTML.lastChild);
+
+  // loading state
+
+  wordsHTML.classList.add("loading");
+
+  await waitOneSecond();
+
+  wordsHTML.classList.remove("loading");
+  if (!word) {
+    word = document.getElementById("search-bar").value;
+  }
+  word.toLowerCase();
+
+
+  // check if a word is entered
 
   if (!word) {
     wordsHTML.innerHTML += `<div class="word">
@@ -15,7 +26,8 @@ const searchWord = async () => {
     return;
   }
 
-  var request;
+  // check if word exists in api
+  let request;
   if (window.XMLHttpRequest) request = new XMLHttpRequest();
   else request = new ActiveXObject("Microsoft.XMLHTTP");
   request.open(
@@ -27,7 +39,7 @@ const searchWord = async () => {
   if (request.status === 404) {
     wordsHTML.innerHTML += `<div class="word">
   <h1 class="word__title">${word}</h1>
-    <div class="word__eachMeaning">${"Cannot find word"}</div>
+    <div class="word__eachMeaning">${"Cannot find word you are searching for"}</div>
   </div>`;
     return;
   }
@@ -39,21 +51,27 @@ const searchWord = async () => {
 
   wordsHTML.innerHTML += `<div class="word">
   <h1 class="word__title">${word}</h1>
-    <div class="word__eachMeaning"></div>
   </div>`;
+
+  console.log(wordData)
 
   displayEachWordMeaning(wordData);
 };
 
+// display meaning of each word
+
 const displayEachWordMeaning = (word) => {
   const eachMeaningHTML = document.querySelector(".words").lastElementChild;
+
+  const phonetic = getPhonetic(); // fix phonetic
+
+  eachMeaningHTML.innerHTML += `<p class="word__phonetics">Phonetic: ${phonetic}</p>`;
+
 
   word.forEach((ele) => {
     const meanings = getWordMeanings(ele.meanings);
 
-    // fix phoneitic?
-    eachMeaningHTML.innerHTML += `<p class="word__phonetics">Phonetic:</p>
-    <div class="word__meaning">
+    eachMeaningHTML.innerHTML += `<div class="word__meaning">
       ${meanings}
     </div>
     `;
@@ -68,7 +86,7 @@ const getWordMeanings = (meanings) => {
 
     returnMeaning += `<div class="word__definition">
     <ul class word__definitions>
-    ${ele.partOfSpeech}
+    <em>${ele.partOfSpeech}</em>
     ${definitions}
     </ul>
     </div>`;
@@ -86,3 +104,26 @@ const getWordDefinitions = (definitions) => {
 
   return returnDefinitions;
 };
+
+const waitOneSecond = () => {
+  return new Promise((resolve) => setTimeout(resolve, 1000));
+};
+
+// for home website
+// keeps word stored and searches once arrives
+const changeWebsites = async () => {
+  let word = document.getElementById("search__input-box").value;
+
+  sessionStorage.setItem("word", word);
+  sessionStorage.setItem("enteredWord", true);
+
+  document.location.href = "http://127.0.0.1:5500/js/dictionaryapi/words.html";
+};
+
+if (sessionStorage.getItem("enteredWord")) {
+  searchWord(sessionStorage.getItem("word"));
+
+  sessionStorage.removeItem("word");
+  sessionStorage.removeItem("enteredWord");
+}
+
